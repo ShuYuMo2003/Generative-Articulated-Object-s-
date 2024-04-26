7528cb7095a0de915e3e5a833db0256ba181cf54
ShuYuMo2003 <244679245@qq.com>
2024-04-26 15:55:42 +0800

    feat(onet): add some file from onet code.

---
+++ utils/ResNet.py
@@ -0,0 +1,161 @@
+'''
+copy from https://github.com/autonomousvision/occupancy_networks by ShuYuMo on 2024/4/6.
+'''
+
+
+import torch
+import torch.nn as nn
+
+from .utils_layers import CBatchNorm1d, CBatchNorm1d_legacy
+
+# Resnet Blocks
+class ResnetBlockFC(nn.Module):
+    ''' Fully connected ResNet Block class.
+
+    Args:
+        size_in (int): input dimension
+        size_out (int): output dimension
+        size_h (int): hidden dimension
+    '''
+
+    def __init__(self, size_in, size_out=None, size_h=None):
+        super().__init__()
+        # Attributes
+        if size_out is None:
+            size_out = size_in
+
+        if size_h is None:
+            size_h = min(size_in, size_out)
+
+        self.size_in = size_in
+        self.size_h = size_h
+        self.size_out = size_out
+        # Submodules
+        self.fc_0 = nn.Linear(size_in, size_h)
+        self.fc_1 = nn.Linear(size_h, size_out)
+        self.actvn = nn.ReLU()
+
+        if size_in == size_out:
+            self.shortcut = None
+        else:
+            self.shortcut = nn.Linear(size_in, size_out, bias=False)
+        # Initialization
+        nn.init.zeros_(self.fc_1.weight)
+
+    def forward(self, x):
+        net = self.fc_0(self.actvn(x))
+        dx = self.fc_1(self.actvn(net))
+
+        if self.shortcut is not None:
+            x_s = self.shortcut(x)
+        else:
+            x_s = x
+
+        return x_s + dx
+
+
+class CResnetBlockConv1d(nn.Module):
+    ''' Conditional batch normalization-based Resnet block class.
+
+    Args:
+        c_dim (int): dimension of latend conditioned code c
+        size_in (int): input dimension
+        size_out (int): output dimension
+        size_h (int): hidden dimension
+        norm_method (str): normalization method
+        legacy (bool): whether to use legacy blocks
+    '''
+
+    def __init__(self, c_dim, size_in, size_h=None, size_out=None,
+                 norm_method='batch_norm', legacy=False):
+        super().__init__()
+        # Attributes
+        if size_h is None:
+            size_h = size_in
+        if size_out is None:
+            size_out = size_in
+
+        self.size_in = size_in
+        self.size_h = size_h
+        self.size_out = size_out
+        # Submodules
+        if not legacy:
+            self.bn_0 = CBatchNorm1d(
+                c_dim, size_in, norm_method=norm_method)
+            self.bn_1 = CBatchNorm1d(
+                c_dim, size_h, norm_method=norm_method)
+        else:
+            self.bn_0 = CBatchNorm1d_legacy(
+                c_dim, size_in, norm_method=norm_method)
+            self.bn_1 = CBatchNorm1d_legacy(
+                c_dim, size_h, norm_method=norm_method)
+
+        self.fc_0 = nn.Conv1d(size_in, size_h, 1)
+        self.fc_1 = nn.Conv1d(size_h, size_out, 1)
+        self.actvn = nn.ReLU()
+
+        if size_in == size_out:
+            self.shortcut = None
+        else:
+            self.shortcut = nn.Conv1d(size_in, size_out, 1, bias=False)
+        # Initialization
+        nn.init.zeros_(self.fc_1.weight)
+
+    def forward(self, x, c):
+        net = self.fc_0(self.actvn(self.bn_0(x, c)))
+        dx = self.fc_1(self.actvn(self.bn_1(net, c)))
+
+        if self.shortcut is not None:
+            x_s = self.shortcut(x)
+        else:
+            x_s = x
+
+        return x_s + dx
+
+
+class ResnetBlockConv1d(nn.Module):
+    ''' 1D-Convolutional ResNet block class.
+
+    Args:
+        size_in (int): input dimension
+        size_out (int): output dimension
+        size_h (int): hidden dimension
+    '''
+
+    def __init__(self, size_in, size_h=None, size_out=None):
+        super().__init__()
+        # Attributes
+        if size_h is None:
+            size_h = size_in
+        if size_out is None:
+            size_out = size_in
+
+        self.size_in = size_in
+        self.size_h = size_h
+        self.size_out = size_out
+        # Submodules
+        self.bn_0 = nn.BatchNorm1d(size_in)
+        self.bn_1 = nn.BatchNorm1d(size_h)
+
+        self.fc_0 = nn.Conv1d(size_in, size_h, 1)
+        self.fc_1 = nn.Conv1d(size_h, size_out, 1)
+        self.actvn = nn.ReLU()
+
+        if size_in == size_out:
+            self.shortcut = None
+        else:
+            self.shortcut = nn.Conv1d(size_in, size_out, 1, bias=False)
+
+        # Initialization
+        nn.init.zeros_(self.fc_1.weight)
+
+    def forward(self, x):
+        net = self.fc_0(self.actvn(self.bn_0(x)))
+        dx = self.fc_1(self.actvn(self.bn_1(net)))
+
+        if self.shortcut is not None:
+            x_s = self.shortcut(x)
+        else:
+            x_s = x
+
+        return x_s + dx
