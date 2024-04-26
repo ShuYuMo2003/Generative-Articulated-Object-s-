8fd1fddf18378ff4496a6b852a1f8280a01aabcf
ShuYuMo2003 <244679245@qq.com>
2024-04-27 02:07:37 +0800

    feat(mesh-fusion): add external mesh fusion

---
+++ point2sdf/prepare_data.py
@@ -0,0 +1,53 @@
+import pymeshlab
+from pathlib import Path
+from rich.progress import track
+from multiprocessing import cpu_count
+import shutil
+import os
+import glob
+
+MESH_FUSION_PATH = Path('/home/shuyumo/research/GAO/external/mesh-fusion')
+
+
+def create_folder(path):
+    try: os.makedirs(str(path))
+    except FileExistsError: pass
+
+def process_obj(objs_dir, out_dir):
+    cpu_cnt = cpu_count()
+
+    total_stemname = [file.stem for file in objs_dir.iterdir() if file.suffix == '.obj']
+
+    transform_dir = out_dir / 'trans'
+
+
+    offs_dir = out_dir / 'offs'
+    create_folder(offs_dir)
+    for stemname in track(total_stemname, description="Convert objs 2 offs"):
+        ms = pymeshlab.MeshSet()
+        ms.load_new_mesh(str(objs_dir / (stemname + '.obj')))
+        ms.save_current_mesh(str(out_dir / 'offs' / (stemname + '.off')))
+
+
+    scaled_dir = out_dir / 'sacled'
+    create_folder(scaled_dir)
+    print('Scaling meshes...')
+    assert os.system(f'''python {MESH_FUSION_PATH / '1_scale.py'} \\
+                        --n_proc {cpu_cnt}                         \\
+                        --in_dir {offs_dir}                        \\
+                        --out_dir {scaled_dir}                     \\
+                        --t_dir {transform_dir} ''') == 0
+    print('done')
+
+
+
+if __name__ == '__main__':
+    in_dir  = Path('/home/shuyumo/research/GAO/dataset/100013/textured_objs')
+    out_dir = Path('/home/shuyumo/research/GAO/point2sdf/output')
+
+    shutil.rmtree(str(out_dir))
+    create_folder(out_dir)
+
+    process_obj(in_dir, out_dir)
+
+    os.system('cp -r output /mnt/d/Research/data/output')
