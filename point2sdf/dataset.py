5feaa112768da278e75980bf6a0a4b206fa7ec40
ShuYuMo2003 <244679245@qq.com>
2024-04-29 18:47:25 +0800

    feat(dataset): validate dataset

--- point2sdf/dataset.py
+++ point2sdf/dataset.py
@@ -1,11 +1,14 @@
 from torch.utils.data import Dataset
 import numpy as np
 import torch
+import math
 
 class PartnetMobilityDataset(Dataset):
-    def __init__(self, path:list[tuple[str, str]], unpackbits=True):
+    def __init__(self, path:list[tuple[str, str]], train_ratio:float,
+                 train:bool, unpackbits=True):
         super().__init__()
-        self.datapath = path
+        train_count = int(len(path) * train_ratio)
+        self.datapath = path[:train_count] if train else path[train_count:]
         self.unpackbits = unpackbits
 
     def __len__(self):
--- point2sdf/train.py
+++ point2sdf/train.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+import random
 from torch.utils.data import DataLoader
 from torch import distributions
 from torch.nn import functional as F
@@ -16,12 +18,57 @@ dataset_root_path = '/home/shuyumo/research/GAO/point2sdf/output'
 batch_size = 8
 lr_rate = 1e-2
 total_epoch = 1000
+train_ratio = 0.9
 
-train_dataset = PartnetMobilityDataset(list(zip(
+dataset_path = list(zip(
     glob(dataset_root_path + '/pointcloud/*'),
     glob(dataset_root_path + '/point/*')
-)))
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+))
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+def validate(encoder, decoder, val_dataloader):
+    losses = []
+    accurancys = []
+    with torch.no_grad():
+        for batch, (cp, sp, occ) in enumerate(val_dataloader):
+            cp = cp.to(device)
+            sp = sp.to(device)
+            occ = occ.to(device)
+
+            c = encoder(cp)
+            logits = decoder(c, sp)
+
+            loss_0 = F.binary_cross_entropy_with_logits(logits, occ, reduction='none')
+            loss = loss_0.sum(-1).mean()
+
+            accurancy = (logits > 0).float().eq(occ).float().mean()
+
+            losses.append(loss)
+            accurancys.append(accurancy)
+    return {
+        'loss': torch.tensor(losses).mean(),
+        'acc': torch.tensor(accurancys).mean()
+    }
+
+
+
+setup_seed(hash('ytq') & ((1 << 32) - 1))
+
+train_dataset = PartnetMobilityDataset(dataset_path,
+                                       train_ratio=train_ratio, train=True)
+train_dataloader = DataLoader(train_dataset,
+                              batch_size=batch_size, shuffle=True)
+
+val_dataset = PartnetMobilityDataset(dataset_path,
+                                       train_ratio=train_ratio, train=False)
+val_dataloader = DataLoader(val_dataset,
+                            batch_size=batch_size, shuffle=True)
 
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -38,9 +85,12 @@ decoder.train()
 losses = []
 
 print('training on device = ', device)
+print('train dataset length = ', len(train_dataset))
+print('valda dataset length = ', len(val_dataset))
 
 for epoch in range(total_epoch):
     batch_loss = []
+    batch_acc = []
     for batch, (cp, sp, occ) in enumerate(train_dataloader):
         cp = cp.to(device)
         sp = sp.to(device)
@@ -49,6 +99,8 @@ for epoch in range(total_epoch):
         c = encoder(cp)
         logits = decoder(c, sp)
 
+        acc = (logits > 0).float().eq(occ).float().mean()
+
         loss_0 = F.binary_cross_entropy_with_logits(logits, occ, reduction='none')
         loss = loss_0.sum(-1).mean()
 
@@ -56,9 +108,16 @@ for epoch in range(total_epoch):
         loss.backward()
         optimizer.step()
         batch_loss.append(loss.item())
+        batch_acc.append(acc.item())
+
+    validation_dict = validate(encoder, decoder, val_dataloader)
+    writer.add_scalar("Loss/val", validation_dict['loss'], epoch)
+    writer.add_scalar("Acc/val", validation_dict['acc'], epoch)
+    writer.add_scalar("Loss/train", torch.tensor(batch_loss).mean(), epoch)
+    writer.add_scalar("Acc/train", torch.tensor(batch_acc).mean(), epoch)
+    writer.flush()
 
     print(f'epoch {epoch} loss = {torch.tensor(batch_loss).mean()}')
-    writer.add_scalar("Loss/train", torch.tensor(batch_loss).mean(), epoch)
-    writer.flush()
+
 
 writer.close()
