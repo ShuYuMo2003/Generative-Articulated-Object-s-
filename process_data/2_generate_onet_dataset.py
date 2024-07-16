b04f212631d1aff035cfe9400b6fc0b3b70bdab1
ShuYuMo2003 <244679245@qq.com>
2024-07-16 22:09:53 +0800

    fix(data process): wssb.

--- process_data/2_generate_onet_dataset.py
+++ process_data/2_generate_onet_dataset.py
@@ -27,9 +27,15 @@ def ply_to_obj(ply_file, obj_file):
     center = (max_bound + min_bound) / 2
     mesh.vertices -= center
 
+    # print('1#', mesh.bounds)
+
     scale = (max_bound - min_bound).max() / (1 - 0.001)
     mesh.vertices /= scale
+
+    # print('2#', mesh.bounds)
+
     mesh.export(obj_file.as_posix(), file_type='obj')
+
 
 def obj_to_wtobj(obj_file, wt_obj_file):
     ok = os.system(f'''{manifold_path.as_posix()}     \\
@@ -44,22 +50,40 @@ def wtobj_to_sdf(wt_obj_file, sdf_file):
         file_type='obj'
     )
     common_args = {
-        'surface_point_method' : 'sample',
-        'sign_method': 'normal',
+        'surface_point_method' : 'scan',
+        'sign_method': 'depth',
     }
     # print('sampling sdf 1 1 1')
-    point, sdf = mesh_to_sdf.sample_sdf_near_surface(wt_obj, number_of_points=10000, **common_args)
+    # print('3#', wt_obj.bounds)
+    point, sdf = mesh_to_sdf.sample_sdf_near_surface(wt_obj, number_of_points=1000, **common_args)
 
-    boxsize = 1
-    uni_point = boxsize * np.random.rand(100000, 3) - (boxsize / 2)
+    in_range_indices = np.all((point >= -0.5) & (point <= 0.5), axis=1)
+    point = point[in_range_indices]
+    sdf = sdf[in_range_indices]
+
+    print("1 max = ", point.max(axis=0))
+    print("1 min = ", point.min(axis=0))
+
+    box_size = 1.05
+    uni_point = box_size * np.random.rand(100000, 3) - (box_size / 2)
+
+    print("2 max = ", uni_point.max(axis=0))
+    print("2 min = ", uni_point.min(axis=0))
     # print('sampling sdf 2 2 2')
+
     uni_sdf = mesh_to_sdf.mesh_to_sdf(wt_obj, uni_point, **common_args)
     # print('done done')
+
     point = np.concatenate([point, uni_point], axis=0)
     sdf = np.concatenate([sdf, uni_sdf], axis=0)
 
-    # print('saved', sdf_file)
+    min_bounds, max_bounds = wt_obj.bounds[0], wt_obj.bounds[1]
+    outside_bounds = np.logical_or(np.any(point < min_bounds, axis=1), np.any(point > max_bounds, axis=1))
+    sdf[outside_bounds] = 1
+
+    print('saved', sdf_file)
     np.savez(sdf_file.as_posix(), point=point, sdf=sdf)
+    return "Done"
 
     # selected_point = point[sdf < 0, ...]
 
@@ -91,14 +115,18 @@ def convert_mesh(ply_file, clear_temp=True):
     obj_to_wtobj(obj_file, wt_obj_file)
     print('Converting to (sdf)', sdf_target_file)
     wtobj_to_sdf(wt_obj_file, sdf_target_file)
+    print('finished')
 
     if clear_temp: shutil.rmtree(temp_dir, ignore_errors=True)
 
 if __name__ == '__main__':
     shutil.rmtree('../dataset/2_onet_dataset', ignore_errors=True)
-    all_ply_files = list(filter(lambda x : x.as_posix()[-3:] == 'ply',
-                            Path('../dataset/1_preprocessed_mesh').iterdir()))
+    # all_ply_files = list(filter(lambda x : x.as_posix()[-3:] == 'ply',
+                            # Path('../dataset/1_preprocessed_mesh/').iterdir()))
+    all_ply_files = [Path('../dataset/1_preprocessed_mesh/USB_1948_1.ply')]
 
+    convert_mesh(all_ply_files[0], False)
+    exit(0)
 
     with Pool(cpu_count() // 2) as p:
         result = [
@@ -109,7 +137,8 @@ if __name__ == '__main__':
         while result:
             for r in result:
                 if r.ready():
-                    result.remove(r)
                     bar.update(1)
+                    assert r.get() == 'Done'
+                    result.remove(r)
             sleep(0.1)
 
