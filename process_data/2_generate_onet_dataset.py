66e22c8efa050bbacaaf9d2c76cd4c623577e55c
ShuYuMo2003 <244679245@qq.com>
2024-07-17 03:40:33 +0800

    tmp(transfer to server)

--- process_data/2_generate_onet_dataset.py
+++ process_data/2_generate_onet_dataset.py
@@ -23,16 +23,15 @@ def ply_to_obj(ply_file, obj_file):
         open(ply_file.as_posix(), 'rb'),
         file_type='ply'
     )
+    print('1# bounding box: ', mesh.bounds)
     (min_bound, max_bound) = mesh.bounds
     center = (max_bound + min_bound) / 2
     mesh.vertices -= center
 
-    # print('1#', mesh.bounds)
-
     scale = (max_bound - min_bound).max() / (1 - 0.001)
     mesh.vertices /= scale
 
-    # print('2#', mesh.bounds)
+    print('2# bounding box: ', mesh.bounds)
 
     mesh.export(obj_file.as_posix(), file_type='obj')
 
@@ -53,50 +52,47 @@ def wtobj_to_sdf(wt_obj_file, sdf_file):
         'surface_point_method' : 'sample',
         'sign_method': 'normal',
     }
-    # print('sampling sdf 1 1 1')
-    # print('3#', wt_obj.bounds)
 
-    # point, sdf = mesh_to_sdf.sample_sdf_near_surface(wt_obj, number_of_points=1000, **common_args)
+    print('3# bounding box: ', wt_obj.bounds)
 
-    # in_range_indices = np.all((point >= -0.5) & (point <= 0.5), axis=1)
-    # point = point[in_range_indices]
-    # sdf = sdf[in_range_indices]
+    point, sdf = mesh_to_sdf.sample_sdf_near_surface(wt_obj, number_of_points=100000, **common_args)
 
-    # print("1 max = ", point.max(axis=0))
-    # print("1 min = ", point.min(axis=0))
+    # Centering and Scaling into [-0.5, 0.5]
+    # mesh_to_sdf: The mesh is first transformed to fit inside the unit sphere.
+    # @see: https://pypi.org/project/mesh-to-sdf/
+    valid_point = point[sdf < 0]
+    max_bounds = valid_point.max(axis=0)
+    min_bounds = valid_point.min(axis=0)
+    center = (max_bounds + min_bounds) / 2
+    point -= center
 
-    box_size = 1.05
-    uni_point = box_size * np.random.rand(110000, 3) - (box_size / 2)
+    scale = (max_bounds - min_bounds).max() / (1 - 0.001)
+    point /= scale
 
-    # print("2 max = ", uni_point.max(axis=0))
-    # print("2 min = ", uni_point.min(axis=0))
-    # print('sampling sdf 2 2 2')
+    # Remove points outside the [-0.5, 0.5] range
+    in_range_indices = np.all((point >= -0.51) & (point <= 0.51), axis=1)
+    point = point[in_range_indices]
+    sdf = sdf[in_range_indices]
 
+    n_points = len(point)
+
+    box_size = 1.005
+    uni_point = box_size * np.random.rand(200000 - n_points, 3) - (box_size / 2)
     uni_sdf = mesh_to_sdf.mesh_to_sdf(wt_obj, uni_point, **common_args)
-    # print('done done')
 
-    point = uni_point # np.concatenate([point, uni_point], axis=0)
-    sdf = uni_sdf # np.concatenate([sdf, uni_sdf], axis=0)
+    point = np.concatenate([point, uni_point], axis=0)
+    sdf = np.concatenate([sdf, uni_sdf], axis=0)
 
-    min_bounds, max_bounds = wt_obj.bounds[0], wt_obj.bounds[1]
-    outside_bounds = np.logical_or(np.any(point < min_bounds, axis=1), np.any(point > max_bounds, axis=1))
-    sdf[outside_bounds] = 1
+    # min_bounds, max_bounds = wt_obj.bounds[0], wt_obj.bounds[1]
+    # outside_bounds = np.logical_or(np.any(point < min_bounds + 0.02, axis=1), np.any(point > max_bounds - 0.02, axis=1))
+    # sdf[outside_bounds] = 1
+
+    print('max: ', point.max(axis=0))
+    print('min: ', point.min(axis=0))
 
     print('saved', sdf_file)
     np.savez(sdf_file.as_posix(), point=point, sdf=sdf)
     return "Done"
-
-    # selected_point = point[sdf < 0, ...]
-
-    # pcd = o3d.geometry.PointCloud()
-    # pcd.points = o3d.utility.Vector3dVector(selected_point)
-    # o3d.io.write_point_cloud(sdf_file.as_posix() + '0.ply', pcd)
-
-    # selected_point = point[sdf > 0, ...]
-
-    # pcd = o3d.geometry.PointCloud()
-    # pcd.points = o3d.utility.Vector3dVector(selected_point)
-    # o3d.io.write_point_cloud(sdf_file.as_posix() + '1.ply', pcd)
 
 
 def convert_mesh(ply_file, clear_temp=True):
@@ -126,6 +122,7 @@ if __name__ == '__main__':
     shutil.rmtree('../dataset/2_onet_dataset', ignore_errors=True)
     all_ply_files = list(filter(lambda x : x.as_posix()[-3:] == 'ply',
                             Path('../dataset/1_preprocessed_mesh/').iterdir()))
+
     # all_ply_files = [Path('../dataset/1_preprocessed_mesh/USB_1948_1.ply')]
 
     # convert_mesh(all_ply_files[0], False)
--- train-onet.py
+++ train-onet.py
@@ -11,7 +11,6 @@ from pathlib import Path
 from rich import print
 
 from torch.utils.data import DataLoader
-from torch import distributions
 from torch.nn import functional as F
 from onet_v2.utils.generate_3d import Generator3D
 from onet_v2.dataset import PartnetMobilityDataset
@@ -33,22 +32,17 @@ def setup_seed(seed):
 
 def compute_loss_n_acc(onet, enc_sp, enc_occ, dec_sp, dec_occ):
     logits, kl_loss, mean_z = onet(enc_sp, enc_occ, dec_sp)
-
     i_loss = F.binary_cross_entropy_with_logits(
             logits, dec_occ, reduction='none')
     loss = i_loss.sum(-1).mean() + kl_loss
-
     acc = ((logits > 0) == dec_occ).float().mean()
-
     return loss, acc, mean_z
 
 def gen_image_from_latent(decoder, mean_z):
     with torch.no_grad():
         mesh = generator.generate_from_latent(decoder, mean_z)
-        # mesh.export('logs/temp-validate.obj')
         plotter = pv.Plotter(off_screen=True)
         try:
-            # pv_mesh = pv.read('logs/temp-validate.obj')
             plotter.add_mesh(mesh)
         except ValueError:
             print('Error in plotter.add_mesh with mesh = ', mesh)
@@ -56,31 +50,6 @@ def gen_image_from_latent(decoder, mean_z):
         plotter.show()
         screenshot = plotter.screenshot()
         return screenshot
-
-def validate(onet, val_dataloader):
-    latents = []
-    losses = []
-    accurancys = []
-    with torch.no_grad():
-        for batch, (enc_sp, enc_occ, dec_sp, dec_occ) in enumerate(val_dataloader):
-            enc_sp = enc_sp.to(device)
-            enc_occ = enc_occ.to(device)
-            dec_sp = dec_sp.to(device)
-            dec_occ = dec_occ.to(device)
-
-            loss, accurancy, mean_z = compute_loss_n_acc(onet, enc_sp, enc_occ, dec_sp, dec_occ)
-
-            latents.append(mean_z)
-
-            losses.append(loss)
-            accurancys.append(accurancy)
-
-    img = gen_image_from_latent(onet.get_decoder(), latents[0][[0], ...])
-    return {
-        'loss': torch.tensor(losses).mean(),
-        'acc': torch.tensor(accurancys).mean(),
-        'img': img
-    }
 
 run = wandb.init(
     project="Pointnet encoder N ONet decoder",
@@ -94,16 +63,8 @@ setup_seed(str2hash(_.seed) & ((1 << 20) - 1))
 
 train_dataset = PartnetMobilityDataset(_.dataset_root_path, train_ratio=_.train_ratio,
                                        selected_categories=_.selected_categories, train=True)
-for dd in train_dataset:
-    (enc_sp, enc_occ, dec_sp, dec_occ) = dd
-    print(enc_sp.shape, enc_occ.shape, dec_sp.shape, dec_occ.shape)
-    break
-exit(0)
-train_dataloader = DataLoader(train_dataset, batch_size=_.batch_size, shuffle=True)
 
-val_dataset = PartnetMobilityDataset(_.dataset_root_path, train_ratio=_.train_ratio,
-                                     selected_categories=_.selected_categories, train=True) # deprecated valiate.
-val_dataloader = DataLoader(val_dataset, batch_size=_.batch_size, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=_.batch_size, shuffle=True, num_workers=6)
 
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -123,7 +84,6 @@ losses = []
 
 print('training on device = ', device)
 print('train dataset length = ', len(train_dataset))
-print('valda dataset length = ', len(val_dataset))
 best_val_acc = -1
 for epoch in tqdm(range(_.total_epoch), desc="Training"):
     onet.train()
@@ -132,19 +92,10 @@ for epoch in tqdm(range(_.total_epoch), desc="Training"):
     batch_acc = []
 
     for batch, (enc_sp, enc_occ, dec_sp, dec_occ) in tqdm(enumerate(train_dataloader), desc="Batch", total=len(train_dataloader)):
-        # print('batch = ', batch)
-
         enc_sp = enc_sp.to(device)
         enc_occ = enc_occ.to(device)
         dec_sp = dec_sp.to(device)
         dec_occ = dec_occ.to(device)
-        torch.save(enc_sp, 'logs/enc_sp.pt')
-        torch.save(enc_occ, 'logs/enc_occ.pt')
-        torch.save(dec_sp, 'logs/dec_sp.pt')
-        torch.save(dec_occ, 'logs/dec_occ.pt')
-        # print(enc_sp.shape, enc_occ.shape, dec_sp.shape, dec_occ.shape)
-
-        # print('moved to gpu')
 
         loss, acc, mean_z = compute_loss_n_acc(onet, enc_sp, enc_occ, dec_sp, dec_occ)
 
@@ -154,19 +105,14 @@ for epoch in tqdm(range(_.total_epoch), desc="Training"):
         batch_loss.append(loss.item())
         batch_acc.append(acc.item())
 
-    validation_dict = validate(onet, train_dataloader)
+    img = gen_image_from_latent(onet.get_decoder(), mean_z[[0], ...])
 
     wandb.log({
-        'val_acc' : validation_dict['acc'],
-        'val_loss' : validation_dict['loss'],
         'train_loss' : torch.tensor(batch_loss).mean(),
         'train_acc' : torch.tensor(batch_acc).mean(),
-        'val_img' : wandb.Image(validation_dict['img'], caption='validation image: val[0]'),
+        'val_img' : wandb.Image(img, caption='validation image: val[0]'),
     })
     print(f'epoch {epoch} loss = {torch.tensor(batch_loss).mean()}')
-    if best_val_acc < validation_dict['acc'] or epoch % 100 == 0:
-        print('save from best_val_acc =', best_val_acc, ' to ', validation_dict['acc'], 'epoch = ', epoch)
-        best_val_acc = validation_dict['acc']
-
+    if epoch % 50 == 0:
         savepath = str(Path(_.checkpoint_output) / f'{epoch}-{best_val_acc}.ptn')
         torch.save(onet, savepath)
